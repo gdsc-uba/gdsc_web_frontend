045fa558a6bea7b6c841b6d630a4be5a62900060
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-11-27 17:20:48 +0100

    feat: added section1 of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,29 @@ const Home = () => {
     return ( 
         <div>
             <Navbar />
-            <section className="container text-center py-5">
-                <h4>Welcome to</h4>
-                <h1>GDSC UBA</h1>
+            <section className="container text-center py-5 header-section">
+                <h4 className="">Welcome to</h4>
+                <h1 className="fw-bolder header-main-title">GDSC UBA</h1>
                 <p>Google Developer Student Club Univerty of Bamenda</p>
+                <button className="btn bbutton">Join us</button>
+
+                <span className="icon1">
+                    <svg width="265" height="265" viewBox="0 0 265 265" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="132.579" y="-15.2929" width="209.122" height="209.122" rx="39.5" transform="rotate(45 132.579 -15.2929)" stroke="#4285F4"/>
+                    </svg>
+                </span>
+
+                <span className="icon2">
+                <svg width="205" height="205" viewBox="0 0 205 205" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle r="102" transform="matrix(1 0 0 -1 102.5 102.5)" stroke="black"/>
+                </svg>
+                </span>
+
+                <span className="icon3">
+                    <svg width="265" height="265" viewBox="0 0 265 265" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="132.579" y="-15.2929" width="209.122" height="209.122" rx="39.5" transform="rotate(45 132.579 -15.2929)" stroke="#4285F4"/>
+                    </svg>
+                </span>
             </section>
         </div>
      );
