22936956bf3eace06e9abdebe56b8672a2ff5f34
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-11-27 15:45:37 +0100

    feat: assets files addded

--- gdsc-web-frontend/src/pages/Home.js
+++ gdsc-web-frontend/src/pages/Home.js
@@ -1,6 +1,15 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
     return ( 
-        <div>Home page</div>
+        <div>
+            <Navbar />
+            <section className="container text-center py-5">
+                <h4>Welcome to</h4>
+                <h1>GDSC UBA</h1>
+                <p>Google Developer Student Club Univerty of Bamenda</p>
+            </section>
+        </div>
      );
 }
  
