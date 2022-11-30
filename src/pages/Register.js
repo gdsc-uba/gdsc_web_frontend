eff5d2455a7b0940b90bb5881ffc1d51c06c23a4
Andy-Kin3 <49214978+Andy-Kin3@users.noreply.github.com>
2022-11-30 11:23:48 +0100

    created register page (#4)
    * feat: added Register.js and Register.css
    
    * feat: Updated Register.js and css

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,25 @@
-const Register = () => {
+import React from 'react'
+import './Register.css'
+
+function Register() {
     return ( 
-        <div>Register page</div>
+        <div className="Register">
+            <div className='image'><img src="Tech Life Blockchain.png" alt="Register logo"/>
+            </div>
+            <img src='./lines 5.svg' alt='lines' className='lines'/>
+            <div className="info">
+                <p className="sign">Sign Up</p>
+                <p className="here">Welcome Here!</p>
+                <label>Email</label><br/>
+                <input type='text' name='email' placeholder='email'/><br/><br/>
+                <label>Password</label><br/>
+                <input type='password' name='password' placeholder='password'/><br/><br/>
+                <label>Confirm Password</label><br/>
+                <input type='password' name='password' placeholder='password'/><br/><br/>
+                <button type='submit' value="">Sign Up</button>
+                <p><center>Already have an account? <a href='login'>Login</a></center></p>
+            </div>
+        </div>
      );
 }
  
