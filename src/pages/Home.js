e780c408b9aa519c7ea32ba4e7a603785ded9e95
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-12-01 04:51:27 +0100

    feat: added section 3 of the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,6 +37,23 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
+            <section className="container my-4 sheight">
+                <span className="bg">
+                    <svg width="205" height="205" viewBox="0 0 205 205" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle r="102" transform="matrix(1 0 0 -1 102.5 102.5)" stroke="black"/>
+                    </svg>
+                </span>
+                <div className="row py-4">
+                    <div className="col-md-6 d-flex align-items-center justify-content-center section-3-inner_left">
+                        <h1 className="stroke-white-text">Our<br /> Events</h1>
+                    </div>
+                    <div className="col-md-6 container py-4 ps-4 section-3-inner_right">
+                        <h1 className="mb-5">Mobile <br />App Dev</h1>
+                        <p className="mb-5">lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempar incididunt</p>
+                    </div>
+                </div>
+            </section>
         </div>
      );
 }
