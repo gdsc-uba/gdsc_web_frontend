45b333c024fb199a14043f15c99183049c66f394
Andy-kin3 <andylodme@gmail.com>
2022-12-01 12:05:50 +0100

    feat: Added react-router-dom

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -11,11 +11,11 @@ function Register() {
                 <p className="sign">Sign Up</p>
                 <p className="here">Welcome Here!</p>
                 <label>Email</label><br/>
-                <input type='text' name='email' placeholder='email'/><br/><br/>
+                <input type='text' name='email' placeholder='email' required/><br/><br/>
                 <label>Password</label><br/>
-                <input type='password' name='password' placeholder='password'/><br/><br/>
+                <input type='password' name='password' placeholder='password' required/><br/><br/>
                 <label>Confirm Password</label><br/>
-                <input type='password' name='password' placeholder='password'/><br/><br/>
+                <input type='password' name='password' placeholder='password' required/><br/><br/>
                 <button type='submit' value="">Sign Up</button>
                 <p><center>Already have an account? <a href='login'>Login</a></center></p>
             </div>
