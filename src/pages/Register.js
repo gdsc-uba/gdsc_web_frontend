d25fc7108e8c0b9336e5162301f02fe07e0e39ce
Andy-Kin3 <49214978+Andy-Kin3@users.noreply.github.com>
2022-12-02 00:49:57 +0100

    Updated Register page responsiveness (#7)
    * feat: added Register.js and Register.css
    
    * feat: Updated Register.js and css
    
    * Feat: Added Tech life blockChain.png to public folder
    
    * feat: Added react-router-dom
    
    * feat: Updated Register.css and js
    
    * Feat: Updated responsiveness for register page
    
    * Feat: Updated Register.css

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,18 +4,18 @@ import './Register.css'
 function Register() {
     return ( 
         <div className="Register">
-            <div className='image'><img src="Tech Life Blockchain.png" alt="Register logo"/>
-            </div>
+            <div className='images'><img src="Tech Life Blockchain.png" alt="Register logo" className='register_image'/>
             <img src='./lines 5.svg' alt='lines' className='lines'/>
+            </div>
             <div className="info">
                 <p className="sign">Sign Up</p>
                 <p className="here">Welcome Here!</p>
                 <label>Email</label><br/>
-                <input type='text' name='email' placeholder='email'/><br/><br/>
+                <input type='text' name='email' placeholder='email' required/><br/><br/>
                 <label>Password</label><br/>
-                <input type='password' name='password' placeholder='password'/><br/><br/>
+                <input type='password' name='password' placeholder='password' required/><br/><br/>
                 <label>Confirm Password</label><br/>
-                <input type='password' name='password' placeholder='password'/><br/><br/>
+                <input type='password' name='password' placeholder='password' required/><br/><br/>
                 <button type='submit' value="">Sign Up</button>
                 <p><center>Already have an account? <a href='login'>Login</a></center></p>
             </div>
