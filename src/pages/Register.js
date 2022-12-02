3856820f0d06c30a14aee09af0297b03d8526062
Andy-kin3 <andylodme@gmail.com>
2022-12-02 11:58:27 +0100

    Feat: Readjusted styling to register scope.

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import './Register.css'
+import styles from './Register.module.css'
 
 function Register() {
     return ( 
-        <div className="Register">
-            <div className='images'><img src="Tech Life Blockchain.png" alt="Register logo" className='register_image'/>
-            <img src='./lines 5.svg' alt='lines' className='lines'/>
+        <div className={styles.r_Register}>
+            <div className={styles.r_images}><img src="Tech Life Blockchain.png" alt="Register logo" className={styles.r_register_image}/>
+            <img src='./lines 5.svg' alt='lines' className={styles.r_lines}/>
             </div>
-            <div className="info">
-                <p className="sign">Sign Up</p>
-                <p className="here">Welcome Here!</p>
+            <div className={styles.r_info}>
+                <p className={styles.r_sign}>Sign Up</p>
+                <p className={styles.r_here}>Welcome Here!</p>
                 <label>Email</label><br/>
                 <input type='text' name='email' placeholder='email' required/><br/><br/>
                 <label>Password</label><br/>
