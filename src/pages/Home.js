b7feaf8443ec4365df59ac3d42fbc02f7da41c19
Leinyuy Etiane <83535251+LL-Etiane@users.noreply.github.com>
2022-12-05 01:45:22 +0100

    Registerpage (#12)
    * feat: added Register.js and Register.css
    
    * feat: Updated Register.js and css
    
    * Feat: Added Tech life blockChain.png to public folder
    
    * feat: Added react-router-dom
    
    * feat: Updated Register.css and js
    
    * Feat: Updated responsiveness for register page
    
    * Feat: Updated Register.css
    
    * Feat: Readjusted styling to register scope.
    
    * fix: homepage styles not working and rearranged assets images in register page'
    
    Co-authored-by: Andy-kin3 <andylodme@gmail.com>
    Co-authored-by: Andy-Kin3 <49214978+Andy-Kin3@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -74,25 +74,25 @@ const Home = () => {
                     </div>
                     <div className="section-5-carousel d-flex justify-content-center">
                         <div className="bcarousel text-center p-4 mx-2" style={{backgroundColor: '#87cefa70'}}>
-                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg"></img>
-                            <h5 className="mt-2">John Doe</h5>
+                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg" alt=''></img>
+                            <h5 className="mt-2" >John Doe</h5>
                             <p>Web Developer</p>
                         </div>
 
                         <div className="bcarousel text-center p-4 mx-2" style={{backgroundColor: '#f0808081'}}>
-                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg"></img>
+                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg" alt=''></img>
                             <h5 className="mt-2">Jane Doe</h5>
                             <p>App Developer</p>
                         </div>
 
                         <div className="bcarousel text-center p-4 mx-2" style={{backgroundColor: '#15739285'}}>
-                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg"></img>
+                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg" alt=''></img>
                             <h5 className="mt-2">John Doe</h5>
                             <p>Web Developer</p>
                         </div>
 
                         <div className="bcarousel text-center p-4 mx-2" style={{backgroundColor: '#87cefa70'}}>
-                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg"></img>
+                            <img className="img-fluid mx-auto" src="https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_960_720.jpg" alt=''></img>
                             <h5 className="mt-2">John Doe</h5>
                             <p>Web Developer</p>
                         </div>
